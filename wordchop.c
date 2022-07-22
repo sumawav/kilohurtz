77e20a6b32c1e7b51820af6cede18d1c4beeb38d
Souma <mondal.souma@gmail.com>
2022-07-22 00:59:42 -0500

    adds error handling

--- wordchop.c
+++ wordchop.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -6,12 +7,18 @@
 
 struct termios orig_termios;
 
+void die(const char *s) {
+	perror(s);
+	exit(1);
+}
+
 void disableRawMode() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+		die("tcsetattr");
 }
 
 void enableRawMode() {
- 	tcgetattr(STDIN_FILENO, &orig_termios);
+ 	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
@@ -19,19 +26,24 @@ void enableRawMode() {
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag &= ~(CS8);
  	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
 
- 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+ 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 int main() {
 	enableRawMode();
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+
+	while (1) {
+		char c = '\0';
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
 		if (iscntrl(c)) {
 			printf("%d\r\n", c);
 		} else {
 			printf("%d ('%c')\r\n", c, c);
 		}
-	};
+		if (c == 'q') break;
+	}
 	return 0;
 }
